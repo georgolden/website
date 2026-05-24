068271f9e8db143728a64e5ea840172f3aa44c29
unnamddd <55639625+unnamddd@users.noreply.github.com>
2026-05-24 02:04:16 +0200

    feat: add 3d hero scene

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://gostudio.dev',
-  integrations: [solidJs(), sitemap()],
+  integrations: [solidJs({exclude: ['**/islands/Hero*']}), sitemap(), react({ include: ['**/islands/Hero*'] })],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'et', 'fi', 'sv', 'fr', 'it', 'uk', 'ru'],
